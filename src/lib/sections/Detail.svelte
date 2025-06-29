8981aa8ad23bca34316c037cd427fe622ae2027e
matohoundev <matohoundev@gmail.com>
2025-06-29 14:15:50 +0200

    Amélioration de la section Detail avec ajout de nouvelles cartes, réorganisation du contenu et mise à jour des styles pour une meilleure présentation et réactivité.

--- src/lib/sections/Detail.svelte
+++ src/lib/sections/Detail.svelte
@@ -10,22 +10,19 @@
 			Avec un design impactant, une structure stratégique et un copywriting affûté.
 		</p>
 	</div>
-	<div
-		class="Detail-content flex flex-col md:flex-row justify-center items-center gap-y-6 md:gap-x-12"
-	>
-		<div class=" relative border-1 bg-white rounded-lg">
+	<div class="Detail-content flex flex-col justify-center items-center gap-y-6 lg:gap-x-12">
+		<div class="detail-card relative border-1 bg-white rounded-lg">
 			<span class="detail-card-title text-2xl font-bold absolute">UX/UI DESIGN</span>
-			<div class="flex gap-x-10 items-center py-3 px-3">
+			<div class="flex flex-col lg:flex-row gap-x-10 items-center pt-7 pb-5 px-5 lg:px-3 lg:py-3">
 				<img
 					src="https://images.unsplash.com/photo-1500522144261-ea64433bbe27?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 					alt="UX/UI Design"
-					class="z-10 rounded-lg shadow-lg w-[300px] h-auto object-cover"
 				/>
-				<div class="flex flex-col gap-y-3">
+				<div class="flex flex-col gap-y-3 pt-5 lg:pt-0">
 					<p class="detail-highlight">
 						Arrêtez de passer inaperçu. <br /> Faites partie de ceux qu'on retient.
 					</p>
-					<div class="detail-features flex flex-col gap-y-2">
+					<div class="detail-features flex flex-col gap-y-2 z-10">
 						<p class="">Design premium-sur-mesure, adieu les templates génériques</p>
 						<p class="">Parcours utilisateur naturel et orienté conversion</p>
 						<p class="">Chaque élément est pensé pour guider, engager et pousser à l'action</p>
@@ -33,12 +30,33 @@
 				</div>
 			</div>
 		</div>
+
+		<div class="detail-card relative border-1 bg-white rounded-lg">
+			<div
+				class="flex flex-col-reverse lg:flex-row justify-between gap-x-10 items-center px-5 pt-5 pb-7 lg:py-3 lg:pr-3 lg:pl-8"
+			>
+				<div class="flex flex-col gap-y-3 pt-5 lg:pt-0">
+					<p class="detail-highlight">
+						Arrêtez de passer inaperçu. <br /> Faites partie de ceux qu'on retient.
+					</p>
+					<div class="detail-features flex flex-col gap-y-2 z-10">
+						<p class="">Design premium-sur-mesure, adieu les templates génériques</p>
+						<p class="">Parcours utilisateur naturel et orienté conversion</p>
+						<p class="">Chaque élément est pensé pour guider, engager et pousser à l'action</p>
+					</div>
+				</div>
+				<img
+					src="https://images.unsplash.com/photo-1500522144261-ea64433bbe27?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					alt="UX/UI Design"
+				/>
+				<span class="detail-card-title-return text-2xl font-bold absolute">ONBOARDING</span>
+			</div>
+		</div>
 	</div>
 </section>
 
 <style>
 	section {
-		height: 100vh;
 		border: 1px solid black;
 		/* background-color: #fbf9f7; */
 		background-color: #ffffff;
@@ -66,7 +84,7 @@
 		line-height: 1.4;
 	}
 
-	@media screen and (min-width: 768px) {
+	@media screen and (min-width: 1024px) {
 		.detail-title-effect {
 			top: 2.5px;
 			left: 2px;
@@ -87,6 +105,40 @@
 		}
 	}
 
+	.detail-card {
+		width: 95%;
+	}
+
+	@media screen and (min-width: 1024px) {
+		.detail-card {
+			width: 85%;
+			max-width: 1000px;
+		}
+	}
+
+	@media screen and (min-width: 1920px) {
+		.detail-card {
+			width: 60%;
+		}
+	}
+
+	img {
+		z-index: 10;
+		border-radius: 0.5rem;
+		box-shadow:
+			0 10px 15px -3px rgb(0 0 0 / 0.1),
+			0 4px 6px -4px rgb(0 0 0 / 0.1);
+		/* width: 300px; */
+		height: auto;
+		object-fit: cover;
+	}
+
+	@media screen and (min-width: 1024px) {
+		img {
+			max-width: 300px;
+		}
+	}
+
 	.detail-card-title {
 		font-family: 'Montserrat', sans-serif;
 		font-weight: bold;
@@ -98,6 +150,17 @@
 		/* border: 1px solid red; */
 		padding: 0.5rem 0 0.5rem 1rem;
 		/* border-radius: 0 0.5rem 0.5rem 0; */
+	}
+
+	.detail-card-title-return {
+		font-family: 'Montserrat', sans-serif;
+		font-weight: bold;
+		font-size: 2.2rem;
+		color: #000;
+		background-color: #fff;
+		bottom: -30px;
+		left: -5px;
+		padding: 0.5rem 1rem 0.5rem 0;
 	}
 
 	.detail-highlight {
